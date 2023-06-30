6c90d39bba0578aba2e4b9e9534c81f5519e6300
Felix Wednesday <felix.dido.4@gmail.com>
2023-06-30 19:50:15 +0400

    fixed typo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/IndividualCourseWork3",
+  publicPath: "/IndividualCourseWork3/",
 });
